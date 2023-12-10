c9a4ef88346ed464c7ba4e5a0a234c689396757e
TroyRandall <troyrandall18@gmail.com>
2023-12-10 13:37:04 -0500

    portfolio first draft finalized, waiting to get feedback before refactoring simple things

--- app.js
+++ app.js
@@ -10,4 +10,17 @@
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
     })
+    document.querySelector("#contactMe").addEventListener("click", () => {
+        document.querySelector(".active-btn").classList.remove("active-btn");
+        document.querySelector("#contactToggle").classList.add("active-btn");
+        document.querySelector('.active').classList.remove("active")
+        document.querySelector("#contact").classList.add("active")
+    })
+
+    document.querySelector("#testimonials").addEventListener("click", () => {
+        document.querySelector(".active-btn").classList.remove("active-btn");
+        document.querySelector("#testimonialsToggle").classList.add("active-btn");
+        document.querySelector('.active').classList.remove("active")
+        document.querySelector("#blogs").classList.add("active")
+    })
 })();
